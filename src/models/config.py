9f8566eccdf5dafec59424deab3bff30b1b9881e
Michael Carroll <mjoecarroll@gmail.com>
2026-07-29 12:26:48 -0400

    Use index list instead of individual fields

--- src/models/config.py
+++ src/models/config.py
@@ -2,7 +2,7 @@
 
 from googleapiclient.model import BaseModel
 
-from src.indexes.sms.models import SMSConfig
+from src.models.indices import BaseIndexConfigModel
 from src.models.rag import MeilisearchConfig
 
 
@@ -10,4 +10,4 @@ class ConfigModel(BaseModel):
     """ArcSearch configuration model."""
 
     meilisearch: MeilisearchConfig
-    text_messages: SMSConfig
+    indices: list[BaseIndexConfigModel]
