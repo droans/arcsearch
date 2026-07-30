cd8ded9132518cc4c70876d2d7233eab2b5ac1ce
Michael Carroll <mjoecarroll@gmail.com>
2026-07-30 14:49:16 -0400

    Add file path constants

---
+++ src/indexers/gmail/const.py
@@ -0,0 +1,14 @@
+"""Constants."""
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataPaths:
+    """Paths for data files."""
+
+    PROCESSING_PATH: Path = Path("processing")
+    METADATA_PATH: Path = Path("meta")
+    FAILED_EMAIL_PROCESSING: Path = Path(PROCESSING_PATH, "emails_failed_processing.json")
+    LAST_PROCESS_TS: Path = Path(METADATA_PATH, "last_download.json")
