120116eae9f4b128a916c1180833b3eac6c7b369
Michael Carroll <mjoecarroll@gmail.com>
2026-07-30 14:48:16 -0400

    Fix: Conversations not importing

--- src/indexers/gmail/utils.py
+++ src/indexers/gmail/utils.py
@@ -1,5 +1,7 @@
 """Utility Functions."""
 
+import logging
+
 from src.indexers.gmail.models import (
     ConversationModel,
     EmailContact,
@@ -7,22 +9,31 @@ from src.indexers.gmail.models import (
     IndexedEmailContact,
 )
 
+logger = logging.getLogger(__name__)
+
 
 def get_all_conversations(account_name: str, messages: list[EmailModel]) -> list[ConversationModel]:
     """Create lists of conversations."""
     all_threads: dict[str, list[EmailContact]] = {}
     for message in messages:
         thread_id = message.thread_id
-        thread_participants = set(all_threads.get(thread_id, []))
-        participants = list(
-            {
-                message.sender,
-                *message.to,
-                *message.cc,
-                *message.bcc,
-            },
-        )
-        [thread_participants.add(participant) for participant in participants]
+        thread_participants = all_threads.get(thread_id, [])
+        participants = [
+            message.sender,
+            *message.to,
+            *message.cc,
+            *message.bcc,
+        ]
+        msg = f"Message {message.id} participants: {participants}"
+        logger.debug(msg)
+        [
+            thread_participants.append(participant)
+            for participant in participants
+            if participant not in thread_participants
+        ]
+        all_threads[thread_id] = thread_participants
+    msg = f"Conversations found: {len(all_threads)}"
+    logger.debug(msg)
     return [
         ConversationModel(thread_id=thread_id, participants=participants, account_name=account_name)
         for thread_id, participants in all_threads.items()
