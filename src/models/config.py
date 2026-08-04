e8885b805f844e5edee8fd969d7a74a0f068403e
Michael Carroll <mjoecarroll@gmail.com>
2026-08-04 14:35:05 -0400

    Add API to config

--- src/models/config.py
+++ src/models/config.py
@@ -6,8 +6,17 @@ from src.models.indexers import BaseIndexerConfigModel
 from src.models.rag import MeilisearchConfig
 
 
+class APIConfig(BaseModel):
+    """ArcSearch API Config."""
+
+    host: str = "127.0.0.1"
+    port: int = 8000
+    allowed_origins: list[str] = ["*"]
+
+
 class ConfigModel(BaseModel):
     """ArcSearch configuration model."""
 
+    api: APIConfig
     meilisearch: MeilisearchConfig
     indices: list[BaseIndexerConfigModel]
